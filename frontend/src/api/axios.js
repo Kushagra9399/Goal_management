ceb88fef12ac4818d54f5d9d262a409c50f88f35
Kushagra9399 <kushagraprajapat9399@gmail.com>
2026-05-19 12:07:09 +0530

    Frontend api updated

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://goal-management-51k4.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
